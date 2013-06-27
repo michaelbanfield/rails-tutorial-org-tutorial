fa704fd3b7078dab140476c91fca0f87cf145f32
Michae <shrapnelbikes@gmail.com>
2013-06-27 23:57:25 +1000

    finishing user model CRUD

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -13,14 +13,14 @@ describe "AuthenticationPages" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email", with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user}
       it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users', href: users_path) }
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+
       it { should_not have_link('Sign in', href: signin_path) }
 
       describe "followed by signout" do
@@ -43,4 +43,29 @@ describe "AuthenticationPages" do
       end
     end
   end
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+      end
+    end
+  end
+
+
 end
